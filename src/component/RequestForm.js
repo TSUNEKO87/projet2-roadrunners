1e0ea0dff53e4dc0819745def982a56dad3dbb54
TSUNEKO <nadinelecpa87@gmail.com>
2020-11-03 17:09:47 +0100

    form doing

--- src/component/RequestForm.js
+++ src/component/RequestForm.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 
 class RequestForm extends Component{
-    render() {
-      return (
-        <div className='request-form'>
-           <h3>request form</h3>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            start: '',
+            end: ''
+          };
+        
+      }
 
-  }
-  
-}
+      handleChange = (e) => {
+        const name= e.target.name  /*renvoie une strings*/
+
+        this.setState({
+          [name]: e.target.value   /*dinamique key creation ES6*/
+        });
+      }
+
+      handleSubmit =(e) => {
+        e.preventDefault()
+         console.log( 'your favorite city is'  +  this.state.start +  'and'  + this.state.end )
+      }   
+      
+      render() {
+        return (
+            <form className='RequestForm' onSubmit={this.handleSubmit}>  {/*onsubmit permet d'ecouter la soumission du formulaire et aussi de permettre l'action d'une api*/}
+                <label htmlFor='start'></label>
+                <input type='text'  value={this.state.start} placeholder='depart' onChange={this.handleChange} id='start' name='start' />
+                <label htmlFor='end'></label>
+                <input type='text' value={this.state.end} placeholder='destination' onChange={this.handleChange} id='end' name='end'/>
+                <input type='submit' value='Start !' />
+            </form>
+        )
+      }
+    }
 
 export default RequestForm;
