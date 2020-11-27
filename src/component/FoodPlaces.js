08278f6802a2973fa44edff4044a185eb4e0214d
pyjamal <limachijonas@gmail.com>
2020-11-27 09:46:25 +0100

    smth change

--- src/component/FoodPlaces.js
+++ src/component/FoodPlaces.js
@@ -2,22 +2,6 @@ import "./DisplayEditor.css";
 
 function FoodPlaces(props) {
   return (
-<<<<<<< HEAD
-    <div>
-      <div className="scroll">
-        {props.info
-          .filter((restaurant) => restaurant.types.includes("food"))
-          .map((filteredPlace) => (
-            <div
-              onClick={() => props.addOneStep(filteredPlace, "food")}
-              className="places-disposition"
-            >
-              <p>{filteredPlace.name}</p>
-              <p>{filteredPlace.vicinity}</p>
-            </div>
-          ))}
-      </div>
-=======
     <div className="scroll">
       {props.info
         .filter((restaurant) => restaurant.types.includes("food"))
@@ -30,7 +14,6 @@ function FoodPlaces(props) {
             <p>{filteredPlace.vicinity}</p>
           </div>
         ))}
->>>>>>> c3816edeac6d4d3a50e393fe5692282057759190
     </div>
   );
 }
